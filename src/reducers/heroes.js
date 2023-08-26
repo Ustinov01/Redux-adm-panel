c90cf18e6977646cbd41107a8f681b6d60732762
Oleg <oleg.ustinov.1993@mail.ru>
2023-08-26 15:35:20 +0600

    reduxtoolkit createslice

--- src/reducers/heroes.js
+++ src/reducers/heroes.js
@@ -13,28 +13,48 @@ const initialState = {
     heroesLoadingStatus: 'idle'
 }
 
-const heroes = createReducer(initialState, builder => {
-    builder
-        .addCase(heroesFetching, state => {
-            state.heroesLoadingStatus = 'loading';
-        })
-        .addCase(heroesFetched, (state, action) => {
-            state.heroesLoadingStatus = 'idle';
-            state.heroes = action.payload;
-        })
-        .addCase(heroesFetchingError, state => {
-            state.heroesLoadingStatus = 'error';
-        })
-        .addCase(heroCreated, (state, action) => {
-            state.heroesLoadingStatus = 'idle';
-            state.heroes.push(action.payload);
-        })
-        .addCase(heroDeleted, (state, action) => {
-            state.heroesLoadingStatus = 'idle';
-            state.heroes = state.heroes.filter(item => item.id !== action.payload)
-        })
-        .addDefaultCase(() => {})
-})
+const heroes = createReducer(initialState, {
+    [heroesFetching]: state => {state.heroesLoadingStatus = 'loading'},
+    [heroesFetched]: (state, action) => {
+                    state.heroesLoadingStatus = 'idle';
+                    state.heroes = action.payload;
+                },
+    [heroesFetchingError]: state => {
+                    state.heroesLoadingStatus = 'error';
+                },
+    [heroCreated]: (state, action) => {
+                    state.heroes.push(action.payload);
+                },
+    [heroDeleted]: (state, action) => {
+                    state.heroes = state.heroes.filter(item => item.id !== action.payload);
+                }
+        },
+    [],
+    state => state
+)
+
+// const heroes = createReducer(initialState, builder => {
+//     builder
+//         .addCase(heroesFetching, state => {
+//             state.heroesLoadingStatus = 'loading';
+//         })
+//         .addCase(heroesFetched, (state, action) => {
+//             state.heroesLoadingStatus = 'idle';
+//             state.heroes = action.payload;
+//         })
+//         .addCase(heroesFetchingError, state => {
+//             state.heroesLoadingStatus = 'error';
+//         })
+//         .addCase(heroCreated, (state, action) => {
+//             state.heroesLoadingStatus = 'idle';
+//             state.heroes.push(action.payload);
+//         })
+//         .addCase(heroDeleted, (state, action) => {
+//             state.heroesLoadingStatus = 'idle';
+//             state.heroes = state.heroes.filter(item => item.id !== action.payload)
+//         })
+//         .addDefaultCase(() => {})
+// })
 
 // const heroes = (state = initialState, action) => {
 //     switch (action.type) {
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import heroes from '../reducers/heroes';
-import filters from '../reducers/filters';
+import heroes from '../components/heroesList/heroesSlice';
+import filters from '../components/heroesFilters/filterSlice';
 
 const stringMiddleware = () => (next) => (action) => {
 
@@ -13,31 +13,6 @@ const stringMiddleware = () => (next) => (action) => {
         return next(action);
 }
 
-
-const enhancer = (createStore) => (...args) => {
-    const store = createStore(...args);
-
-    const oldDispatch = store.dispatch;
-    store.dispatch = (action) => {
-        if(typeof action === 'string') {
-            return oldDispatch({
-                type: action
-            })
-        } 
-        return oldDispatch(action); 
-    }
-
-    return store;
-}
-
-// const store = createStore(combineReducers({heroes, filters}),
-//               compose(applyMiddleware(ReduxThunk, stringMiddleware),
-//                       window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
-                // compose(
-                //     enhancer,
-                //     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-
-                // ));
 
 const store = configureStore({
     reducer: {heroes, filters},
